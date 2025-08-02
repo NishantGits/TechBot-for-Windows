b5946362d654b81aba520c35eb998a3ece9247ff
NishantGits <nlrayyanyo@gmail.com>
2025-08-02 13:54:38 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 800,
-    autoHideMenuBar: true,
+    icon: path.join(__dirname, 'icon.ico'), // <-- Add this line
     webPreferences: {
-      contextIsolation: true
+      nodeIntegration: false,
     }
   });
 
-  win.loadURL('https://www.chatbase.co/chatbot-iframe/uDZF2kjdPDEcdNrF2z_Y1');
+  win.loadURL('https://www.chatbase.co');
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
